3c51dc074e7a59d056706bbbe3d6b5cf51900384
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2023-12-07 16:32:46 +0000

    Basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,14 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+	"""
+        Base class for all other sub classes.
+        Defines all common attributes and methods for subclasses
+    """
+    
 	def __init__(self, *args, **kwargs):
+    	""" initializes self """
+     
 		self.id = str(uuid.uuid4())
 		self.created_at = datetime.now
 		self.updated_at = datetime.now
