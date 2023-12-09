a09b337fc1663c1c98832c2911ec81aa0a427936
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2023-12-09 04:28:59 +0000

    Reveiwing task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-
+"""Defining the BaseModel class."""
+import models
 import uuid
 from datetime import datetime
+
 
 class BaseModel:
     """
@@ -23,18 +25,22 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            models.storage.new(self)
+            
+            
     def __str__(self):
         """A string representation of the instance."""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
     def save(self):
+        """Update the updated_at attribute and save the instance."""
         self.updated_at = datetime.now()
+        models.storage.save()
     
     def to_dict(self):
         """Returns a dictionary containing all keys/values."""
-        obj_dict = self.__dict__.copy()
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        obj_dict["__class__"] = self.__class__.__name__
-        return obj_dict
+        v_dict = self.__dict__.copy()
+        v_dict["created_at"] = self.created_at.isoformat()
+        v_dict["updated_at"] = self.updated_at.isoformat()
+        v_dict["__class__"] = self.__class__.__name__
+        return v_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 import json
+from os import path
 from models.base_model import BaseModel
 
 class FileStorage:
@@ -33,7 +34,7 @@ class FileStorage:
         with open(self.__file_path, 'w') as file:
             json.dump(objects_serialized, file)
         
-       def reload(self):
+    def reload(self):
         """
         Deserializes the JSON file to __objects.
         Only if the JSON file (__file_path) exists; otherwise, do nothing.
@@ -42,9 +43,12 @@ class FileStorage:
         if path.exists(self.__file_path):
             with open(self.__file_path, 'r', encoding="utf-8") as file:
                objects_serialized = json.load(file)
-               for prm_key, obj_dt in objects_serialized.items():
-                   cls_name, obj_id = key.split('-')
-                   # Create an instance of the class dynamically based on cls_name
-                   
+               for key, obj_dt in objects_serialized.items():
+                    cls_name, obj_id = key.split('-')
+                    # Create an instance of the class dynamically based on cls_name
+                    obj_class = globals()[class_name]
+                    obj_instance = obj_class(**obj_data)
+                    # Store the instance in __objects
+                    self.__objects[key] = obj_instance
         
         
