1d4c7daa3e632f0665f9bb09b751d2ae32b6125b
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-12-10 21:09:37 -0800

    Revert "pycodestyle"
    This reverts commit 31647e6312f56a29ec288acb4d06517e001b79f9.

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """A string representation of the instance."""
@@ -36,6 +35,7 @@ class BaseModel:
         """Update the updated_at attribute and save the instance."""
         self.updated_at = datetime.now()
         models.storage.save()
+        models.storage.new(self)
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values."""
