d032a9bddbec07d8b18abf4355c5dc075ffc9b25
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-12-10 19:51:26 -0800

    Base

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,12 @@
 import json
 from os import path
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -14,7 +20,15 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
-    cls_dict = {"BaseModel": BaseModel}
+    cls_dict = {
+            'BaseModel': BaseModel
+            'User': User,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
+            }
     
     def all(self):
         """Returns the dictionary objects."""
